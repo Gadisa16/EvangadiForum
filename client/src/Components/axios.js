673ec5461a2350ccb6249b850dcfbd3055521082
Gadisa16 <133113137+Gadisa16@users.noreply.github.com>
2024-07-18 16:18:10 +0300

    baseURL is updated

--- client/src/Components/axios.js
+++ client/src/Components/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 const axiosBase = axios.create({
-    baseURL:'http://localhost:3333/api'
+    // baseURL:'http://localhost:3333/api'
+    baseURL: "https://evangadiforum-3-36du.onrender.com"
 })
 export default axiosBase
