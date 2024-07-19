7d766552f1905bd274b40423400b82861763877c
Gadisa16 <133113137+Gadisa16@users.noreply.github.com>
2024-07-19 10:09:36 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,39 +1,43 @@
 const express = require('express');
 const app = express();
 
-const cors = require('cors')
-require("dotenv").config()
-app.use(cors())
+const cors = require('cors');
+require('dotenv').config();
+app.use(cors());
 
-//db connection
+// db connection
 const dbConnection = require('./db/dbConfig');
 
-//user routes middleware file
+// user routes middleware file
 const userRoutes = require('./routes/userRoutes');
 const questionRoute = require('./routes/questionRoutes');
 const answerRoute = require('./routes/answerRoutes');
 const authMiddleware = require('./middleware/authMiddleware');
 
-//json middleware to extract to json data
+// json middleware to extract to json data
 app.use(express.json());
 
-//user routes middleware
-app.use("/api/users", userRoutes);
-//question route middleware
-app.use("/api/questions", authMiddleware, questionRoute)
-//answer route middleware
-app.use("/api/answers", authMiddleware, answerRoute)
+// user routes middleware
+app.use('/api/users', userRoutes);
+// question route middleware
+app.use('/api/questions', authMiddleware, questionRoute);
+// answer route middleware
+app.use('/api/answers', authMiddleware, answerRoute);
 
-// const port = 3333;
-const port = process.env.PORT || 3333;
-async function start(){
-    try {
-        const result = await dbConnection.execute("select 'test'")
-        await app.listen(port)
-        console.log("database connection established")
-        console.log(`listening on ${port}`)
-    } catch (error) {
-        console.log(error.message)
-    }
+const port = process.env.PORT || 3000;
+
+async function start() {
+  try {
+    const result = await dbConnection.execute("select 'test'");
+    console.log('database connection established');
+  } catch (error) {
+    console.error('Database connection error:', error.message);
+    process.exit(1); // Exit process with failure
+  }
+
+  app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+  });
 }
-start()
+
+start();
